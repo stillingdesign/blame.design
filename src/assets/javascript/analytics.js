5b69899a5d9d521a3d225e647d19c07a9ced64e5
Mike Stilllling <mike@stillingdigital.com>
2024-05-06 06:58:49 -0500

    add scroll distance tracking

--- src/assets/javascript/analytics.js
+++ src/assets/javascript/analytics.js
@@ -1,33 +1,58 @@
 // Scroll Depth Tracking Sections
 function scrollDepth() {
-    document.addEventListener('DOMContentLoaded', function () {
-        const sections = document.querySelectorAll('section');
-        const observedSections = new Set(); // Set to keep track of observed sections
+    const sections = document.querySelectorAll('section');
+    const observedSections = new Set(); // Set to keep track of observed sections
 
-        const observerOptions = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.5 // Trigger when 50% of the section is in view
-        };
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5 // Trigger when 50% of the section is in view
+    };
 
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    const index = Array.from(sections).indexOf(entry.target);
-                    // Check if the section has not been logged before
-                    if (!observedSections.has(index)) {
-                        plausible(`Viewed section ${index + 1}`);
-                        console.log("Section " + (index + 1) + " is in view");
-                        observedSections.add(index); // Add to the set to prevent re-logging
-                    }
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const index = Array.from(sections).indexOf(entry.target);
+                // Check if the section has not been logged before
+                if (!observedSections.has(index)) {
+                    plausible(`Viewed section ${index + 1}`);
+                    console.log("Section " + (index + 1) + " is in view");
+                    observedSections.add(index); // Add to the set to prevent re-logging
                 }
-            });
-        }, observerOptions);
-
-        sections.forEach(section => {
-            observer.observe(section);
+            }
         });
+    }, observerOptions);
+
+    sections.forEach(section => {
+        observer.observe(section);
     });
 }
 
-scrollDepth();
+// Scroll Depth Tracking Percentage
+function scrollDist() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            // Check if the marker has been logged before
+            if (entry.isIntersecting && entry.target.dataset.logged === "false") {
+                plausible(`Read ${entry.target.style.top}`);
+                console.log(`Read ${entry.target.style.top}`);
+                entry.target.dataset.logged = "true"; // Update the logged status
+            }
+        });
+    }, {
+        root: null, // observing for viewport
+        rootMargin: '0px',
+        threshold: 0.5 // Trigger when 50% of the target is visible
+    });
+
+    const markers = document.querySelectorAll('[data-analytics-marker]');
+    markers.forEach(marker => {
+        observer.observe(marker);
+    });
+}
+
+// Run
+document.addEventListener("DOMContentLoaded", function () {
+    scrollDepth();
+    if(document.querySelector('[data-page="blog-post"]')) { scrollDist(); }
+});
